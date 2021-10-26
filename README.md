6b302c1d677a073cb30f5f5decb2092858d61fcb
GVE Devnet Admin <gve-devnet.gen@cisco.com>
2021-10-26 03:27:30 +0000

    Baselined from internal Repository last_commit:c96001378b0ac965b841f95c4e1bae727afe9c23

---
+++ README.md
@@ -0,0 +1,85 @@
+# Meraki Network Configuration Script 
+
+The Meraki Network Configuration Script provides an programmable way to make network configuration changes on the meraki network by using the Meraki API. 
+
+## Contacts
+* Eda Akturk 
+
+## Solution Components
+* Meraki Dashboard API
+* Meraki MS  
+* Meraki MX
+* Python 
+
+## Installation:
+
+#### Clone the repo
+```
+$ git clone https://github.com/gve-sw/gve_devnet_meraki_networkconfigurations.git
+```
+
+#### Set up a Python venv
+First make sure that you have Python 3 installed on your machine. We will then be using venv to create 
+an isolated environment with only the necessary packages.
+
+##### Install virtualenv via pip
+```
+$ pip install virtualenv
+```
+
+##### Create a new venv
+```
+Change to your project folder
+$ cd gve_devnet_meraki_networkconfigurations
+
+Create the venv
+$ virtualenv venv
+
+Activate your venv
+$ source venv/bin/activate
+```
+
+#### Install dependencies
+```
+$ pip install -r requirements.txt
+```
+
+## Setup:
+
+1. Add your meraki api key to main.py. 
+```
+api_key = " "
+```
+
+2. You can add your network_id if you would like to perform changes to a single network. (You could also get all the networks if you would like to loop through them)
+```
+network_id = " "
+```
+
+## Usage
+
+Edit the main function with the network calls you would like to test and run the script by the following command:
+
+
+    $ python main.py
+
+
+You can check the results from the meraki dashboard. 
+
+![/IMAGES/0image.png](/IMAGES/0image.png)
+
+### LICENSE
+
+Provided under Cisco Sample Code License, for details see [LICENSE](LICENSE.md)
+
+### CODE_OF_CONDUCT
+
+Our code of conduct is available [here](CODE_OF_CONDUCT.md)
+
+### CONTRIBUTING
+
+See our contributing guidelines [here](CONTRIBUTING.md)
+
+#### DISCLAIMER:
+<b>Please note:</b> This script is meant for demo purposes only. All tools/ scripts in this repo are released for use "AS IS" without any warranties of any kind, including, but not limited to their installation, use, or performance. Any use of these scripts and tools is at your own risk. There is no guarantee that they have been through thorough testing in a comparable environment and we are not responsible for any damage or data loss incurred with their use.
+You are responsible for reviewing and testing any scripts you run thoroughly before use in any non-testing environment.
---
+++ main.py
@@ -0,0 +1,230 @@
+import json
+import requests
+
+base_url = "https://api.meraki.com/api/v1/"
+api_key = " "
+network_id = " "
+
+
+def get_organizations():
+    url = f"{base_url}organizations"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def get_networks(organization_id):
+    url = f"{base_url}organizations/{organization_id}/networks"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def get_devices(network_id):
+    url = f"{base_url}networks/{network_id}/devices"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def get_vlan(network_id):
+    url = f"{base_url}networks/{network_id}/appliance/vlans"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def create_vlan(network_id, vlan, vlan_name, subnet, device_id):
+    """
+    Create vlan: https://developer.cisco.com/meraki/api-v1/#!create-network-appliance-vlan
+    """
+    url = f"{base_url}networks/{network_id}/appliance/vlans"
+    payload = {
+        "id": vlan,
+        "name": vlan_name,
+        "subnet": subnet,
+        "applianceIp": device_id
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('POST', url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+
+def update_vlan(network_id, vlan, appliance_ip):
+    """
+    Update network applince vlan: https://developer.cisco.com/meraki/api-v1/#!update-network-appliance-vlan
+    """
+    url = f"{base_url}networks/{network_id}/appliance/vlans/{vlan}"
+    payload = {
+        "applianceIp": appliance_ip
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('PUT', url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+
+def get_network_appliance_ports(network_id):
+    url = f"https://api.meraki.com/api/v1/networks/{network_id}/appliance/ports"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def update_network_appliance_port(network_id, port_id, vlan):
+    """
+    Update Network Appliance Port: https://developer.cisco.com/meraki/api-v1/#!update-network-appliance-port
+    """
+    url = f"https://api.meraki.com/api/v1/networks/{network_id}/appliance/ports/{port_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    payload = {
+        "enabled": "true",
+        "type": "access",
+        "dropUntaggedTraffic": "false",
+        "vlan": vlan,
+        "accessPolicy": "open"
+    }
+    response = requests.request('PUT', url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+
+def get_network_switch_settings(serial):
+    url = f"{base_url}devices/{serial}/switch/ports"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def update_network_switch_settings(serial, port_id, vlan):
+    """
+    Update Network Switch Settings: https://developer.cisco.com/meraki/api-v1/#!update-network-switch-settings
+    """
+    url = f"{base_url}devices/{serial}/switch/ports/{port_id}"
+    payload = {
+        "name": "My switch port",
+        "vlan": vlan
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+
+    response = requests.request('PUT', url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+
+def get_network_switch_static_routes(serial):
+    url = f"{base_url}devices/{serial}/switch/routing/staticRoutes"
+    payload = None
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    return response.json()
+
+
+def update_network_switch_static_routes(serial, static_route_id, name, subnet, next_hop_ip):
+    """
+    Update Network Switch Static Route: https://developer.cisco.com/meraki/api-v1/#!update-device-switch-routing-static-route
+    """
+    url = f"{base_url}devices/{serial}/switch/routing/staticRoutes/{static_route_id}"
+    payload = {
+        "name": name,
+        "subnet": subnet,
+        "nextHopIp": next_hop_ip
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Cisco-Meraki-API-Key": api_key
+    }
+    response = requests.request('PUT', url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+
+if __name__ == '__main__':
+    # Use the functions above to make config changes on the Meraki network
+
+    devices = get_devices(network_id)
+    print(devices)
+
+    """
+    # Sample code to create vlan:
+    
+    vlan = "1002"
+    vlan_name = "Test from python"
+    subnet = "192.168.1.0/24"
+    device_id = "192.168.1.2"
+    add_vlan = create_vlan(network_id, vlan, vlan_name,subnet,device_id)
+
+
+    # Sample code to get the static routes of all the devices: 
+
+    devices = get_devices(network_id)
+    for device in devices:
+        serial = device["serial"]
+        static_routes = get_network_switch_static_routes(serial)
+        msg = f"Device: {serial} the static routes are:"
+        print(msg)
+        for route in static_routes:
+            route_id = route["staticRouteId"]
+            print(route_id)
+            
+
+    # Sample code to update network switch static route:
+
+    serial = ' '
+    static_routes = get_network_switch_static_routes(serial)
+    print(static_routes)
+    static_route_id = " "
+    subnet = "0.0.0.0/0"
+    next_hop = "192.168.128.1"
+    new_static_route = update_network_switch_static_routes(serial, static_route_id, "python test", subnet, next_hop)
+    print(new_static_route)
+    """
